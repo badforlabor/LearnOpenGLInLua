dfcd9d03064a6436c0e6e0a3fc0e37b39c0ddc1b
badlabor <badlabor@work.com>
2017-06-01 19:22:13 +0800

    环境贴图例子

---
+++ advanced_opengl/6.2.cubemaps_environment_mapping.lua
@@ -0,0 +1,317 @@
+-- 深度测试
+
+print('_VERSION = ' .. _VERSION)
+
+require 'luaglew'
+print('luaglew.VERSION = '   .. luaglew.VERSION)
+
+require 'luaglut'
+print('luaglut.VERSION = ' .. luaglut.VERSION)
+
+
+require 'luaglm'
+print('luaglm.VERSION = '   .. luaglm.VERSION)
+
+require 'lib/libre_util'
+require 'lib/Camera'
+require 'lib/Shader'
+
+
+local camera = Camera:new();
+camera.Position.x = 0;
+camera.Position.y = 0;
+camera.Position.z = 3;
+camera:updateCameraVectors();
+
+lightPos = glm.vec3:new(1.2, 1, 2.0);
+
+local quit = false
+local fps = 60
+local msec = 1000 / fps
+
+local lightingShader, skyboxShader;
+
+local vbo, cubeVAO;
+local skyboxVBO, skyboxVAO;
+
+local screen_width = 800;
+local screen_height = 600;
+
+local currentfile = 'advanced_opengl/6.2.cubemaps_environment_mapping'
+-- local currentfile = 'lighting/1.colors'
+
+local LastX, LastY;
+
+local skyboxVertices = 
+  {
+        -1.0,  1.0, -1.0,
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0,  1.0, -1.0,
+        -1.0,  1.0, -1.0,
+
+        -1.0, -1.0,  1.0,
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+        -1.0, -1.0,  1.0,
+
+         1.0, -1.0, -1.0,
+         1.0, -1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0, -1.0,
+         1.0, -1.0, -1.0,
+
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+
+        -1.0,  1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0,
+
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+         1.0, -1.0,  1.0
+  };
+local cubeTexture, cubemapTexture; 
+  
+
+function resize_func(w, h)
+  --[[
+   local ratio = w / h
+   glMatrixMode(GL_PROJECTION)
+   glLoadIdentity()
+   ]]--
+   print('viewport:' .. w .. ',' .. h);
+   glViewport(0,0,w,h);
+   screen_width = w;
+   screen_height = h;
+   --[[
+   gluPerspective(45,ratio,1,1000)
+   glMatrixMode(GL_MODELVIEW)
+   glLoadIdentity()
+   set_material_clay()
+   ]]--
+   --glEnable(GL_DEPTH_TEST)
+   --glEnable(GL_NORMALIZE)
+end
+
+function timer_func()
+   if quit then return end
+   
+      glutSetWindow(window)
+      glutTimerFunc(msec, timer_func, 0)
+      glutPostRedisplay()
+end
+
+function display_func()
+  if quit then return end
+  
+  glEnable(GL_DEPTH_TEST);
+  glClearColor(0.1, 0.1, 0.1, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT + GL_DEPTH_BUFFER_BIT)
+  
+  lightingShader:use();  
+  
+  
+
+  local projection = glm.perspective(glm.radians(camera.Zoom), 800 / 600.0, 0.1, 100);
+  local view = camera:GetViewMatrix();
+  lightingShader:setMat4("projection", projection);
+  lightingShader:setMat4("view", view);
+  
+  local model = glm.mat4:new();
+
+  lightingShader:SetInt("skybox", 0);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+  
+  -- cubes
+  model = glm.translate(model, glm.vec3:new(-1,0,-1));
+  lightingShader:SetMat4("model", model);
+  lightingShader:SetVec3("cameraPos", camera.Position);
+  glBindVertexArray(cubeVAO);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+
+  -- 天空盒，后绘制天空盒的目的是，做ztest的时候，能过直接剔除掉被“cubes”挡住的部分。
+  glDepthFunc(GL_LEQUAL);
+  skyboxShader:use();
+  skyboxShader:setMat4("projection", projection);
+  view = glm.mat4:new(glm.mat3:new(camera:GetViewMatrix()));
+  skyboxShader:setMat4("view", view);
+  skyboxShader:SetInt("texture1", 0);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+  glBindVertexArray(skyboxVAO);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glDepthFunc(GL_LESS);  
+  
+  glutSwapBuffers()
+end
+
+function mouse_func(button, updown, x, y)
+  LastX = x;
+  LastY = y;
+  -- print(string.format('button:%d, updown:%d, x:%d, y:%d', button, updown, x, y));
+end
+
+function motion_func(x, y)
+  -- print(string.format('x:%d, y:%d', x, y));
+  camera:ProcessMouseMovement(x - LastX, LastY - y);
+  LastX = x;
+  LastY = y;  
+end
+
+-- press ESC to exit
+function keyboard_func(key,x,y)
+  
+  print('key' .. key);
+  
+   if key == 27 then
+      quit = true
+      glutDestroyWindow(window)
+      os.exit(0)
+   end
+   
+    if key == 119 then
+      camera:ProcessKeyboard(0)
+    end
+    if key == 115 then
+      camera:ProcessKeyboard(1)
+    end
+    if key == 97 then
+      camera:ProcessKeyboard(2)
+    end
+    if key == 100 then
+      camera:ProcessKeyboard(3)
+    end
+    
+    if key == 91 then
+      camera:ProcessMouseScroll(1);
+    end
+    if key == 93 then
+      camera:ProcessMouseScroll(-1);
+    end
+end
+
+glutInit(arg)
+glutInitDisplayMode(GLUT_RGB + GLUT_DOUBLE + GLUT_DEPTH)
+if arg then title = arg[0] else title = "glut" end
+window = glutCreateWindow(title)
+glutReshapeWindow(800,600);
+
+-- >> init glew and shader
+glewInit()
+glEnable(GL_DEPTH_TEST);
+-- glDepthFunc(GL_ALWAYS);
+glDepthFunc(GL_LESS);
+
+lightingShader = Shader:new(currentfile .. '.vs', currentfile .. '.fs');
+skyboxShader = Shader:new(currentfile .. '.skybox.vs', currentfile .. '.skybox.fs');
+-- << init end
+
+-- >> 准备数据，绘制一个三角形
+local VBO_ARRAY = glGenBuffers(3);
+local VAO_ARRAY = glGenVertexArrays(3);
+vbo = VBO_ARRAY[1];
+cubeVAO = VAO_ARRAY[1];
+skyboxVBO = VBO_ARRAY[2];
+skyboxVAO = VAO_ARRAY[2];
+quadVBO = VBO_ARRAY[3];
+quadVAO = VAO_ARRAY[3];
+local vertices = 
+    {
+    --  // positions     //  normal
+        -0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+         0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+         0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+         0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+        -0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+        -0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+
+        -0.5, -0.5,  0.5,  0.0,  0.0, 1.0,
+         0.5, -0.5,  0.5,  0.0,  0.0, 1.0,
+         0.5,  0.5,  0.5,  0.0,  0.0, 1.0,
+         0.5,  0.5,  0.5,  0.0,  0.0, 1.0,
+        -0.5,  0.5,  0.5,  0.0,  0.0, 1.0,
+        -0.5, -0.5,  0.5,  0.0,  0.0, 1.0,
+
+        -0.5,  0.5,  0.5, -1.0,  0.0,  0.0,
+        -0.5,  0.5, -0.5, -1.0,  0.0,  0.0,
+        -0.5, -0.5, -0.5, -1.0,  0.0,  0.0,
+        -0.5, -0.5, -0.5, -1.0,  0.0,  0.0,
+        -0.5, -0.5,  0.5, -1.0,  0.0,  0.0,
+        -0.5,  0.5,  0.5, -1.0,  0.0,  0.0,
+
+         0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+         0.5,  0.5, -0.5,  1.0,  0.0,  0.0,
+         0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+         0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+         0.5, -0.5,  0.5,  1.0,  0.0,  0.0,
+         0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+
+        -0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+         0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+         0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+         0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+        -0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+        -0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+
+        -0.5,  0.5, -0.5,  0.0,  1.0,  0.0,
+         0.5,  0.5, -0.5,  0.0,  1.0,  0.0,
+         0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+         0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+        -0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+        -0.5,  0.5, -0.5,  0.0,  1.0,  0.0
+    };
+
+
+glBindBuffer(GL_ARRAY_BUFFER, vbo);
+glBufferFormatedData(GL_ARRAY_BUFFER, GL_FLOAT, vertices, GL_STATIC_DRAW);
+
+glBindVertexArray(cubeVAO);
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof_float(), 0);
+glEnableVertexAttribArray(0);
+glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof_float(), 3 * sizeof_float());
+glEnableVertexAttribArray(1);
+
+glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+glBufferFormatedData(GL_ARRAY_BUFFER, GL_FLOAT, skyboxVertices, GL_STATIC_DRAW);
+
+glBindVertexArray(skyboxVAO);
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof_float(), 0);
+glEnableVertexAttribArray(0);
+-- << 数据准备完毕
+
+cubeTexture = LoadTexture("resources/textures/marble.jpg");
+local faces = {
+  "resources/textures/skybox/right.jpg",
+  "resources/textures/skybox/left.jpg",
+  "resources/textures/skybox/top.jpg",
+  "resources/textures/skybox/bottom.jpg",
+  "resources/textures/skybox/back.jpg",
+  "resources/textures/skybox/front.jpg"
+};
+cubemapTexture = LoadCubemap(faces);
+
+glutDisplayFunc(display_func)
+glutKeyboardFunc(keyboard_func)
+glutReshapeFunc(resize_func)
+glutTimerFunc(msec, timer_func, 0)
+glutMouseFunc(mouse_func);
+glutMotionFunc(motion_func);
+
+glutMainLoop()
--- 创建文件.bat
+++ 创建文件.bat
@@ -1,12 +1,12 @@
-set src=5.1.framebuffers
-set dst=6.1.cubemaps_skybox
+set src=6.1.cubemaps_skybox
+set dst=6.2.cubemaps_environment_mapping
 
 cd advanced_opengl
 
 copy /y %src%.lua %dst%.lua
 copy /y %src%.vs %dst%.vs
 copy /y %src%.fs %dst%.fs
-copy /y %src%_screen.vs %dst%.skybox.vs
-copy /y %src%_screen.fs %dst%.skybox.fs
+copy /y %src%.skybox.vs %dst%.skybox.vs
+copy /y %src%.skybox.fs %dst%.skybox.fs
 
 ::pause
