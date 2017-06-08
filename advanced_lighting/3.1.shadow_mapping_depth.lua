348371cae29728f146a5ab5677b9e4f98abb7bbd
badlabor <badlabor@work.com>
2017-06-08 19:15:57 +0800

    渲染框架整理

--- advanced_lighting/3.1.shadow_mapping_depth.lua
+++ advanced_lighting/3.1.shadow_mapping_depth.lua
@@ -1,35 +1,8 @@
 -- 深度测试
 
-print('_VERSION = ' .. _VERSION)
+require 'lib/MyWindowFramework'
 
-require 'luaglew'
-print('luaglew.VERSION = '   .. luaglew.VERSION)
-
-require 'luaglut'
-print('luaglut.VERSION = ' .. luaglut.VERSION)
-
-require 'luaglm'
-print('luaglm.VERSION = '   .. luaglm.VERSION)
-
-require 'luamesh'
-print('luamesh.VERSION = '   .. luamesh.VERSION)
-
-require 'lib/libre_util'
-require 'lib/Camera'
-require 'lib/Shader'
-
-local camera = Camera:new();
-camera.Position.x = 0;
-camera.Position.y = 0;
-camera.Position.z = 3;
-camera:updateCameraVectors();
 local lightPos = glm.vec3:new(-2,4,-1);
-
-local quit = false
-local fps = 60
-local msec = 1000 / fps
-local SCREEN_WIDTH = 1280;
-local SCREEN_HEIGHT = 720;
 
 local myshader, debugShader;
 
@@ -37,8 +10,6 @@ local vbo, vao;
 
 local currentfile = 'advanced_lighting/3.1.shadow_mapping_depth'
 -- local currentfile = 'lighting/1.colors'
-
-local LastX, LastY;
 
 local planeVertices = 
 {
@@ -59,7 +30,11 @@ local depthMapFBO, depthMap;
 local nearPlane = 1.0
 local farPlane = 7.5;
 
-function SetData()
+function OnInit()
+
+  SCREEN_WIDTH = 800;
+  SCREEN_HEIGHT = 600;
+
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -241,10 +216,7 @@ function renderScene(tempShader)
   renderCube();
 end
 
-
-
-function display_func()
-  if quit then return end
+function OnDraw()
   
   glClearColor(0.1, 0.1, 0.1, 1.0);
   glClear(GL_COLOR_BUFFER_BIT + GL_DEPTH_BUFFER_BIT)
@@ -276,101 +248,12 @@ function display_func()
   glBindTexture(GL_TEXTURE_2D, depthMap);
   renderQuad();
 
-  glutSwapBuffers()
 end
   
-
-function resize_func(w, h)
-  --[[
-   local ratio = w / h
-   glMatrixMode(GL_PROJECTION)
-   glLoadIdentity()
-   ]]--
-   print('viewport:' .. w .. ',' .. h);
-   glViewport(0,0,w,h)
-   --[[
-   gluPerspective(45,ratio,1,1000)
-   glMatrixMode(GL_MODELVIEW)
-   glLoadIdentity()
-   set_material_clay()
-   ]]--
-   --glEnable(GL_DEPTH_TEST)
-   --glEnable(GL_NORMALIZE)
-end
-
-function timer_func()
-   if quit then return end
-   
-      glutSetWindow(window)
-      glutTimerFunc(msec, timer_func, 0)
-      glutPostRedisplay()
-end
-
-function mouse_func(button, updown, x, y)
-  LastX = x;
-  LastY = y;
-  -- print(string.format('button:%d, updown:%d, x:%d, y:%d', button, updown, x, y));
-end
-
-function motion_func(x, y)
-  -- print(string.format('x:%d, y:%d', x, y));
-  camera:ProcessMouseMovement(x - LastX, LastY - y);
-  LastX = x;
-  LastY = y;  
-end
 
 -- press ESC to exit
-function keyboard_func(key,x,y)
-  
-  print('key' .. key);
-  
-   if key == 27 then
-      quit = true
-      glutDestroyWindow(window)
-      os.exit(0)
-   end
-   
-    if key == 119 then
-      camera:ProcessKeyboard(0)
-    end
-    if key == 115 then
-      camera:ProcessKeyboard(1)
-    end
-    if key == 97 then
-      camera:ProcessKeyboard(2)
-    end
-    if key == 100 then
-      camera:ProcessKeyboard(3)
-    end
-    
-    if key == 91 then
-      camera:ProcessMouseScroll(1);
-    end
-    if key == 93 then
-      camera:ProcessMouseScroll(-1);
-    end
-
-    -- 按键'B'
-    if key == 98 then blinn = (blinn + 1) % 2 end;
+function OnKey(key)      
+  if key == 'b' then blinn = (blinn + 1) % 2 end;
 end
 
-glutInit(arg)
-glutInitDisplayMode(GLUT_RGB + GLUT_DOUBLE + GLUT_DEPTH)
-if arg then title = arg[0] else title = "glut" end
-window = glutCreateWindow(title)
-glutReshapeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
-
--- >> init glew and shader
-glewInit()
--- << init end
-
-SetData();
-
-glutDisplayFunc(display_func)
-glutKeyboardFunc(keyboard_func)
-glutReshapeFunc(resize_func)
-glutTimerFunc(msec, timer_func, 0)
-glutMouseFunc(mouse_func);
-glutMotionFunc(motion_func);
-
-glutMainLoop()
+glMain();
---
+++ lib/MyWindowFramework.lua
@@ -0,0 +1,158 @@
+
+-- 基本的渲染框架
+-- 引用该框架之后，只需要定义函数 OnInit(), OnKey(key), OnDraw(), OnQuit() 函数即可。
+
+print('_VERSION = ' .. _VERSION)
+
+require 'luaglew'
+print('luaglew.VERSION = '   .. luaglew.VERSION)
+
+require 'luaglut'
+print('luaglut.VERSION = ' .. luaglut.VERSION)
+
+require 'luaglm'
+print('luaglm.VERSION = '   .. luaglm.VERSION)
+
+require 'luamesh'
+print('luamesh.VERSION = '   .. luamesh.VERSION)
+
+require 'lib/libre_util'
+require 'lib/Camera'
+require 'lib/Shader'
+
+mainCamera = Camera:new();
+mainCamera.Position.x = 0;
+mainCamera.Position.y = 0;
+mainCamera.Position.z = 3;
+mainCamera:updateCameraVectors();
+
+SCREEN_WIDTH = 1280;
+SCREEN_HEIGHT = 720;
+
+local quit = false
+local fps = 60
+local msec = 1000 / fps
+
+-- local currentfile = 'lighting/1.colors'
+
+local LastX;
+local LastY;
+
+function display_func()
+  if quit then return end
+  
+  -- 外部接口，绘制
+  if OnDraw then OnDraw(); end
+
+  glutSwapBuffers()
+end
+
+function OnPressKey(key)
+    if (OnKey) then OnKey(key); end
+end
+  
+
+function resize_func(w, h)
+  --[[
+   local ratio = w / h
+   glMatrixMode(GL_PROJECTION)
+   glLoadIdentity()
+   ]]--
+   print('viewport:' .. w .. ',' .. h);
+   glViewport(0,0,w,h)
+   --[[
+   gluPerspective(45,ratio,1,1000)
+   glMatrixMode(GL_MODELVIEW)
+   glLoadIdentity()
+   set_material_clay()
+   ]]--
+   --glEnable(GL_DEPTH_TEST)
+   --glEnable(GL_NORMALIZE)
+end
+
+function timer_func()
+   if quit then return end
+      glutSetWindow(window)
+      glutTimerFunc(msec, timer_func, 0)
+      glutPostRedisplay()
+end
+
+function mouse_func(button, updown, x, y)
+  LastX = x;
+  LastY = y;
+  -- print(string.format('button:%d, updown:%d, x:%d, y:%d', button, updown, x, y));
+end
+
+function motion_func(x, y)
+  -- print(string.format('x:%d, y:%d', x, y));
+  mainCamera:ProcessMouseMovement(x - LastX, LastY - y);
+  LastX = x;
+  LastY = y;  
+end
+
+-- press ESC to exit
+function keyboard_func(key,x,y)
+  
+  print('key' .. key);
+  
+   if key == 27 then
+      quit = true
+      glutDestroyWindow(window)
+      os.exit(0)
+   end
+   
+    if key == 119 then
+      mainCamera:ProcessKeyboard(0)
+    end
+    if key == 115 then
+      mainCamera:ProcessKeyboard(1)
+    end
+    if key == 97 then
+      mainCamera:ProcessKeyboard(2)
+    end
+    if key == 100 then
+      mainCamera:ProcessKeyboard(3)
+    end
+    
+    if key == 91 then
+      mainCamera:ProcessMouseScroll(1);
+    end
+    if key == 93 then
+      mainCamera:ProcessMouseScroll(-1);
+    end
+
+    -- 按键'B'
+    if key == 98 then OnPressKey('b') end;
+end
+
+function glMain()
+
+    glutInit(arg)
+    glutInitDisplayMode(GLUT_RGB + GLUT_DOUBLE + GLUT_DEPTH)
+    if arg then title = arg[0] else title = "glut" end
+    window = glutCreateWindow(title)
+
+    -- >> init glew and shader
+    glewInit()
+    -- << init end
+
+    -- 显示电脑的GL版本号，如果版本号小于4，会显示异常（shader不支持）
+    print('GL Version=' .. glGetString(GL_VERSION))
+
+    -- 外部接口，准备数据
+    if OnInit then OnInit(); end
+
+    glutReshapeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    glutDisplayFunc(display_func)
+    glutKeyboardFunc(keyboard_func)
+    glutReshapeFunc(resize_func)
+    glutTimerFunc(msec, timer_func, 0)
+    glutMouseFunc(mouse_func);
+    glutMotionFunc(motion_func);
+
+    glutMainLoop()
+
+    -- 游戏退出后，清理
+    if OnQuit then OnQuit(); end
+end
